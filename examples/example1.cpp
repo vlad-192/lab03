f0747d16dcefb8fe5c1b7b2c49561a70ecac63a7
vlad-192 <vladlotnik825@gmail.com>
2026-05-13 19:38:56 +0300

    remove unnecessary files

--- examples/example1.cpp
+++
@@ -1,6 +0,0 @@
-#include <print.hpp>
-
-int main()
-{
-  print("hello");
-}
--- examples/example2.cpp
+++
@@ -1,9 +0,0 @@
-#include <print.hpp>
-
-#include <fstream>
-
-int main()
-{
-  std::ofstream file("log.txt");
-  print(std::string("hello"), file);
-}
